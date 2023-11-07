83aae264c979e7d6dace006726092549c4c5422d
Rachel S <136056290+Rachel2705@users.noreply.github.com>
2023-11-07 12:06:54 +0530

    Create Transcription_Translation.py

---
+++ Transcription_Translation.py
@@ -0,0 +1,37 @@
+def transcribe(dna_sequence):
+    return dna_sequence.replace('T', 'U')
+
+def translate(rna_sequence):
+    codon_table = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',
+        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+    }
+    protein = ""
+    for i in range(0, len(rna_sequence), 3):
+        codon = rna_sequence[i:i+3]
+        amino_acid = codon_table.get(codon, '')
+        if amino_acid == '*':
+            break
+        protein += amino_acid
+    return protein
+
+# Example usage:
+dna_sequence = "ATGGCCATGGCGCCCAGAACTGAGATCAATAGTACCCGTATTAACGGGTGA"
+rna_sequence = transcribe(dna_sequence)
+protein = translate(rna_sequence)
+print(f"RNA Sequence: {rna_sequence}")
+print(f"Protein Sequence: {protein}")
